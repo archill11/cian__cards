27b846cfe832ff557c56ca16e2694a0b93cafacb
Artur <artur2000sis@gmail.com>
2022-08-21 19:06:53 +0300

    finish second card

--- js/script.js
+++ js/script.js
@@ -3,3 +3,24 @@ const likeBtn = document.querySelector('.header__likeBtn')
 likeBtn.addEventListener('click', () => {
     likeBtn.classList.toggle('header__likeBtn--active')
 })
+
+// translateX radiobuttons
+const radioWrapperAbsolut = document.querySelector('.type__radio-wrapper--absolut')
+let x1 = null;
+
+function handleTouchStart(e) {
+    const firstTouch = e.touches[0];
+    x1 = firstTouch.clientX;
+}
+
+function handleTouchMove(e) {
+    if (!x1) {
+        return
+    }
+    let x2 = e.touches[0].clientX
+    let xDiff = x2 - x1
+
+    radioWrapperAbsolut.style.transform = `translateX(${xDiff}px)`
+}
+radioWrapperAbsolut.addEventListener('touchstart', handleTouchStart, false);
+radioWrapperAbsolut.addEventListener('touchmove', handleTouchMove, false);
